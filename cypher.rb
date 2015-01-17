b43f5330ba453c041a6fec7211623c5195e71478
gluten <goulvenschaal@me.com>
2015-01-17 11:44:15 +0100

    Make it works conveniently with Thor

--- cypher.rb
+++ cypher.rb
@@ -8,19 +8,33 @@ require 'thor'
 
 class Cypher < Thor
 
-  desc "init", "Get the code_words list from Json"
-  def init
-    file       = File::open("wordlist.json")
-    contents   = file.read
+  no_commands {
+    # Get the code_words list from Json
+    def parse_codes
+      file     = File::open("wordlist.json")
+      contents = file.read
+      return JSON.parse(contents)
+    end
 
-    return JSON.parse(contents)
-  end
+    # Save the jibberish to a new file
+    def save(idea)
+      print "File encoded. Please enter a name for this idea: "
+      idea_name = $stdin.gets.strip
+      File::open( "idea-" + idea_name + ".md", "w" ) do |f|
+        f << idea
+      end
+    end
+  }
 
+  # CLI Methods
+  # ===========
 
-  desc "New ideas", "Get evil ideas and swap in code words"
-  def new_idea(code_words)
+  desc "new", "Get evil ideas and swap in code words"
+  def new_idea
     print "Enter your new idea: "
-    idea = gets
+    idea = $stdin.gets
+
+    code_words = parse_codes
 
     # Substitute words with code_words list
     code_words.each do |word, substitution|
@@ -30,19 +44,13 @@ class Cypher < Thor
     save(idea)
   end
 
-  desc "Save idea", "Save the jibberish to a new file"
-  def save(idea)
-    print "File encoded. Please enter a name for this idea: "
-    idea_name = gets.strip
-    File::open( "idea-" + idea_name + ".md", "w" ) do |f|
-      f << idea
-    end
-  end
-
-  desc "Show ideas", "Print each idea out with the words fixed"
+  desc "show", "Print each idea out with the words fixed"
   def show_ideas
-    Dir['idea-*.txt'].each do |file_name|
+    Dir['idea-*.md'].each do |file_name|
       idea = File.read( file_name )
+
+      code_words = parse_codes
+
       code_words.each do |real, code|
         idea.gsub!( code, real)
       end
